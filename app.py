0a4487a329a2b955141f081af73f697ce5026d89
rex <cse22105@iiitklayani.ac.in>
2024-03-13 23:01:08 +0530

    seprated the css file for each section

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
+from dbConnector import UseDatabase
+
+dbconfig = {'host': '127.0.0.1', 'user': 'root', 'password': '97082', 'database': 'oops'}
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage() -> 'Html page':
-    return render_template('homepage.html', activehome='active')
+    return render_template('homepage.html', activehome='active',css='static/homepagecss.css')
 
 @app.route('/product')
 def product() -> 'Html Page':
-
-    return render_template('product.html', activeproduct='active')
+    return render_template('product.html', activeproduct='active',css='static/productcss.css')
 
 @app.route('/about_us')
 def about_us() -> 'Html Page':
@@ -20,5 +22,11 @@ def about_us() -> 'Html Page':
 def webapp() -> 'Html Page':
 
     return render_template('webapp.html', activewebapp='active')
+
+@app.route('/tool')
+def tool() -> 'Name of Diseases':
+
+    return render_template('webapp.html', activewebapp='active')
+
 
 app.run(debug=True)
